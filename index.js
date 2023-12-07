80e6f990ea3ef2a7ddf90030271c102ad60d593f
ThisisHappyEL <1379This_is_HappyEL@mail.ru>
2023-12-07 21:46:07 +0700

    Выполнил все шаги из 10 урока

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import _ from "lodash";
 
+import sumTwoNumber from "./src/index.js";
+
 console.log("Hello, Hexlet!");
 
 console.log(_.last(["one", "two"]));
+
+console.log(sumTwoNumber(2, 5));
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const sumTwoNumber = (num1, num2) => (num1 + num2);
+
+console.log(sumTwoNumber(3, 4));
+
+export default sumTwoNumber;
